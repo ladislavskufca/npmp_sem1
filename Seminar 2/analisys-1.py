537939aadfffc056fd942de41ad125cc833546e2
Luka Podgorsek <lp5796@student.uni-lj.si>
2019-01-03 17:33:41 +0100

    analiza gostote & difuzije

--- Seminar 2/analisys-1.py
+++ Seminar 2/analisys-1.py
@@ -45,5 +45,107 @@ def analyse_diffusion_rate_and_density(size, number_of_examples=150):
     plt.show()
 
 
+def analyse_diffusion_rate(size=10, number_of_examples=150):
+    """
+    Vpliv hitrosti difuzije na oscilacije pri podani velikosti prostora
+    :return:
+    """
+    example_num = 0
+    x_per = []
+    y_per = []
+
+    x_amp = []
+    y_amp = []
+
+    for d in np.linspace(start=.01, stop=.8, num=number_of_examples):
+        rep = r.Repressilator_S_PDE()
+        rep.set_params(D1=d, density=0.4, size=size)
+        [osc, freq, period, amplitude, damped] = rep.run()
+
+        print("#%d | d: %f, | oscillates: %d, frequency: %f, period: %f, amplitude: %f" % (
+            example_num, d, osc, freq, period, amplitude))
+
+        if osc:
+            # period graph
+            x_per.append(period)
+            y_per.append(d)
+            # amplitude graph
+            x_amp.append(amplitude)
+            y_amp.append(d)
+
+        example_num += 1
+
+    # # draw graph
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1, 1, 1)
+    # ax.plot(x_amp, y_amp, 'r', x_per, y_per, 'b')
+    # ax.set_title('Vplih hitrosti difuzije na oscilacije pri podani velikosti prostora')
+    #
+    # # display the plot
+    # # plt.show()
+    #
+    # print(x_amp)
+    # print(y_amp)
+
+    plt.subplot(2, 1, 1)
+    plt.title('Vpliv hitrosti difuzije na amplitudo v prostoru %dx%d' % (size, size))
+    plt.plot(y_amp, x_amp)
+    plt.ylabel('Amplituda')
+    plt.xlabel('Hitrost difuzije')
+
+    plt.subplot(2, 1, 2)
+    plt.title('Vpliv hitrosti difuzije na periodo v prostoru %dx%d' % (size, size))
+    plt.plot(y_per, x_per)
+    plt.ylabel('Perioda')
+    plt.xlabel('Hitrost difuzije')
+    plt.show()
+
+
+def analyse_density(size=10, number_of_examples=150):
+    """
+    Vpliv hitrosti difuzije na oscilacije pri podani velikosti prostora
+    :return:
+    """
+    example_num = 0
+    x_per = []
+    y_per = []
+
+    x_amp = []
+    y_amp = []
+
+    for density in np.linspace(start=.1, stop=1, num=number_of_examples):
+        rep = r.Repressilator_S_PDE()
+        rep.set_params(density=density, size=size)
+        [osc, freq, period, amplitude, damped] = rep.run()
+
+        print("#%d | density: %f, | oscillates: %d, frequency: %f, period: %f, amplitude: %f" % (
+            example_num, density, osc, freq, period, amplitude))
+
+        if osc:
+            # period graph
+            x_per.append(period)
+            y_per.append(density)
+            # amplitude graph
+            x_amp.append(amplitude)
+            y_amp.append(density)
+
+        example_num += 1
+
+    plt.subplot(2, 1, 1)
+    plt.title('Vpliv gostote celic na amplitudo v prostoru %dx%d' % (size, size))
+    plt.plot(y_amp, x_amp)
+    plt.ylabel('Amplituda')
+    plt.xlabel('Gostota')
+
+    plt.subplot(2, 1, 2)
+    plt.title('Vpliv gostote celic na periodo v prostoru %dx%d' % (size, size))
+    plt.plot(y_per, x_per)
+    plt.ylabel('Perioda')
+    plt.xlabel('Gostota')
+    plt.show()
+
+
 if __name__ == "__main__":
-    analyse_diffusion_rate_and_density(15)
+    # analyse_diffusion_rate_and_density(15)
+    # analyse_diffusion_rate(number_of_examples=20)
+    analyse_density(number_of_examples=20)
