7a3d9d40fbcd8e4f5ce723f1a3cc35e6f2b6f617
Luka Podgorsek <lp5796@student.uni-lj.si>
2019-01-12 18:35:07 +0100

    dodana frekvenca

--- Seminar 2/analisys-1.py
+++ Seminar 2/analisys-1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import spline
+from scipy.ndimage.filters import gaussian_filter1d
 
 import represilator as r
 
@@ -23,13 +25,15 @@ def analyse_diffusion_rate_and_density(size, number_of_examples=150):
     for d in np.linspace(start=.01, stop=3, num=number_of_examples):
         for density in np.linspace(start=.1, stop=1, num=5):
             rep = r.Repressilator_S_PDE()
-            rep.load_params()
             rep.set_params(D1=d, density=density, size=size)
             # check if model oscillates
-            oscillates = rep.run()[0]
-            print("#%d | d: %f, density: %f, oscillates: %d" % (example_num, d, density, oscillates))
+            [osc, freq, period, amplitude, damped] = rep.run()
+            print("#%d | density: %f, | oscillates: %d, frequency: %f, period: %f, amplitude: %f" % (
+                example_num, density, osc, freq, period, amplitude))
+
             example_num += 1
-            if oscillates:
+            del rep
+            if osc:
                 x.append(d)
                 y.append(density)
             else:
@@ -39,7 +43,7 @@ def analyse_diffusion_rate_and_density(size, number_of_examples=150):
     print(x)
     print(y)
     plt.title('Vpliv hitrosti difuzije na oscilacije v prostoru %dx%d' % (size, size))
-    plt.plot(x, y, 'ro')
+    plt.plot(x, y)
     plt.ylabel('Gostota')
     plt.xlabel('Hitrost difuzije')
     plt.show()
@@ -114,6 +118,9 @@ def analyse_density(size=10, number_of_examples=150):
     x_amp = []
     y_amp = []
 
+    x_freq = []
+    y_freq = []
+
     for density in np.linspace(start=.1, stop=1, num=number_of_examples):
         rep = r.Repressilator_S_PDE()
         rep.set_params(density=density, size=size)
@@ -130,32 +137,46 @@ def analyse_density(size=10, number_of_examples=150):
             x_amp.append(amplitude)
             y_amp.append(density)
 
+            x_freq.append(freq)
+            y_freq.append(density)
+
         example_num += 1
         del rep
 
-    plt.subplot(2, 1, 1)
+    plt.subplot(3, 1, 1)
     plt.title('Vpliv gostote celic na amplitudo v prostoru %dx%d' % (size, size))
-    plt.plot(y_amp, x_amp)
+    # x_smooth = np.linspace(np.array(x_amp).max(), np.array(x_amp).min(), number_of_examples)
+    # y_smooth = spline(x_amp, y_amp, x_smooth)
+    y_smooth = gaussian_filter1d(y_amp, sigma=2)
+    plt.plot(y_smooth, x_amp)
     plt.ylabel('Amplituda')
     plt.xlabel('Gostota')
 
-    plt.subplot(2, 1, 2)
+    plt.subplot(3, 1, 2)
     plt.title('Vpliv gostote celic na periodo v prostoru %dx%d' % (size, size))
-    plt.plot(y_per, x_per)
+    y_smooth = gaussian_filter1d(y_per, sigma=2)
+    plt.plot(y_smooth, x_per)
     plt.ylabel('Perioda')
+    plt.xlabel('Gostota')
+
+    plt.subplot(3, 1, 3)
+    plt.title('Vpliv gostote celic na frekvenco v prostoru %dx%d' % (size, size))
+    y_smooth = gaussian_filter1d(y_freq, sigma=2)
+    plt.plot(y_smooth, x_freq)
+    plt.ylabel('Frekvenca')
     plt.xlabel('Gostota')
     plt.show()
 
 
 if __name__ == "__main__":
-    # analyse_diffusion_rate_and_density(15)
+    analyse_diffusion_rate_and_density(size=10, number_of_examples=5)
 
     # Vpliv hitrosti difuzije na oscilacije pri podani velikosti prostora
-    analyse_diffusion_rate(number_of_examples=20, size=10)
-    analyse_diffusion_rate(number_of_examples=20, size=15)
-    analyse_diffusion_rate(number_of_examples=20, size=20)
+    # analyse_diffusion_rate(number_of_examples=10, size=10)
+    # analyse_diffusion_rate(number_of_examples=20, size=15)
+    # analyse_diffusion_rate(number_of_examples=20, size=20)
 
     # Vpliv gostote celic na oscilacije pri podani velikosti prostora
-    analyse_density(number_of_examples=20, size=10)
-    analyse_density(number_of_examples=20, size=15)
-    analyse_density(number_of_examples=20, size=20)
+    # analyse_density(number_of_examples=150, size=10)
+    # analyse_density(number_of_examples=150, size=15)
+    # analyse_density(number_of_examples=150, size=20)
