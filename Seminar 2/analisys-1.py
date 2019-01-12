a6c74911e4fbf5f3481aa605775b1e3ce3513df6
Luka Podgorsek <lp5796@student.uni-lj.si>
2019-01-12 20:06:32 +0100

    toplotna karta oscilacij

--- Seminar 2/analisys-1.py
+++ Seminar 2/analisys-1.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import spline
 from scipy.ndimage.filters import gaussian_filter1d
 
 import represilator as r
+from image_annotated_heatmap import annotate_heatmap, heatmap
 
 
 def analyse_diffusion_rate_and_density(size, number_of_examples=150):
@@ -19,11 +19,13 @@ def analyse_diffusion_rate_and_density(size, number_of_examples=150):
     """
     x = []
     y = []
+    result = []
     example_num = 0
 
     """Vpliv hitrosti difuzije na oscilacije"""
-    for d in np.linspace(start=.01, stop=3, num=number_of_examples):
-        for density in np.linspace(start=.1, stop=1, num=5):
+    for density in np.linspace(start=.1, stop=1, num=5):
+        tmp = []
+        for d in np.linspace(start=.01, stop=3, num=number_of_examples):#.1 1
             rep = r.Repressilator_S_PDE()
             rep.set_params(D1=d, density=density, size=size)
             # check if model oscillates
@@ -33,20 +35,22 @@ def analyse_diffusion_rate_and_density(size, number_of_examples=150):
 
             example_num += 1
             del rep
-            if osc:
-                x.append(d)
-                y.append(density)
-            else:
-                break
 
-    # draw graph
-    print(x)
-    print(y)
-    plt.title('Vpliv hitrosti difuzije na oscilacije v prostoru %dx%d' % (size, size))
-    plt.plot(x, y)
-    plt.ylabel('Gostota')
-    plt.xlabel('Hitrost difuzije')
-    plt.show()
+            y.append(d)
+            x.append(density)
+            tmp.append(osc)
+        result.append(tmp)
+
+    result = np.array(result)
+
+    x = np.unique(x)
+    x = list(np.around(x, decimals=2))
+
+    y = np.unique(y)
+    y = list(np.around(y, decimals=2))
+
+    # draw heat map
+    make_heatmap(x, y, result)
 
 
 def analyse_diffusion_rate(size=10, number_of_examples=150):
@@ -80,18 +84,7 @@ def analyse_diffusion_rate(size=10, number_of_examples=150):
         example_num += 1
         del rep
 
-    # # draw graph
-    # fig = plt.figure()
-    # ax = fig.add_subplot(1, 1, 1)
-    # ax.plot(x_amp, y_amp, 'r', x_per, y_per, 'b')
-    # ax.set_title('Vplih hitrosti difuzije na oscilacije pri podani velikosti prostora')
-    #
-    # # display the plot
-    # # plt.show()
-    #
-    # print(x_amp)
-    # print(y_amp)
-
+    # draw graph
     plt.subplot(2, 1, 1)
     plt.title('Vpliv hitrosti difuzije na amplitudo v prostoru %dx%d' % (size, size))
     plt.plot(y_amp, x_amp)
@@ -168,8 +161,31 @@ def analyse_density(size=10, number_of_examples=150):
     plt.show()
 
 
+def make_heatmap(x, y, data, y_label='Gostota', x_label='Hitrost difuzije', title='Toplotna karta oscilacij'):
+    """
+    Draw heat map
+    :param title: graph title
+    :param x_label: x label title
+    :param y_label: y label title
+    :param x: x values array
+    :param y: y values array
+    :param data: numpy data array
+    :return:
+    """
+    fig, ax = plt.subplots()
+
+    im, cbar = heatmap(data, x, y, ax=ax, cmap="YlGn", cbarlabel="Oscilira [DA/NE]")
+    texts = annotate_heatmap(im, valfmt="{x:d}")
+
+    fig.tight_layout()
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    plt.title(title)
+    plt.show()
+
+
 if __name__ == "__main__":
-    analyse_diffusion_rate_and_density(size=10, number_of_examples=5)
+    analyse_diffusion_rate_and_density(number_of_examples=20, size=15)
 
     # Vpliv hitrosti difuzije na oscilacije pri podani velikosti prostora
     # analyse_diffusion_rate(number_of_examples=10, size=10)
